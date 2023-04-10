4be2a62bedf2dd24abdbf553fba5f82be3cce2a4
LeonardoScatolin <leonardo.scatolin20@gmail.com>
2023-04-10 11:13:32 -0300

    base criada

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from 'dotenv'
+import mustache from "mustache-express";
+import path from "path";
+import mainRoutes from './routes/index'
+
+dotenv.config()
+
+const server = express();
+
+// configurando template engine
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+// configurando arquivos estáticos
+server.use(express.static(path.join(__dirname, '../public')));
+
+// configurando rotas
+server.use(mainRoutes)
+
+// página não encontrada
+server.use((req, res) => {
+    res.send('página não encontrada')
+})
+
+
+server.listen(process.env.PORT, () => {
+    console.log('http://localhost')
+});
